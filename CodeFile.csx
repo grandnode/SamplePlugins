e3903ed56b8945558c7a68048e7e7a53acfca7c5
Grzegorz Ociepka <grzegorzociepka09@gmail.com>
2019-05-24 09:49:30 +0200

    Added new scripts, updated ExamplePlugin

---
+++ CodeFile.csx
@@ -0,0 +1,22 @@
+﻿#! "netcoreapp2.2"
+#r "Grand.Core"
+#r "Grand.Services"
+
+using System;
+using Grand.Core.Domain.Messages;
+using Grand.Core.Domain.Orders;
+using Grand.Services.Events;
+
+/* Sample code to add new token message (message email) to the order */
+
+public class OrderTokenTest : IConsumer<EntityTokensAddedEvent<Order>>
+{
+
+    public void HandleEvent(EntityTokensAddedEvent<Order> eventMessage)
+    {
+        eventMessage.LiquidObject.AdditionalTokens.Add("NewOrderNumber", $"{eventMessage.Entity.CreatedOnUtc.Year}/{eventMessage.Entity.OrderNumber}");
+        //in message templates you can put new token {{AdditionalTokens["NewOrderNumber"]}}
+    }
+}
+
+
---
+++ Grand.Plugin.Misc.ExamplePlugin/AdminTabsExample.cs
@@ -0,0 +1,31 @@
+﻿using Grand.Framework.Events;
+using Grand.Services.Events;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grand.Plugin.Misc.ExamplePlugin
+{
+    public class AdminTabsExample : IConsumer<AdminTabStripCreated>
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AdminTabsExample(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public Task HandleEvent(AdminTabStripCreated eventMessage)
+        {
+            if (eventMessage.TabStripName == "category-edit")
+            {
+                var categoryId = Convert.ToString(_httpContextAccessor.HttpContext.GetRouteValue("ID"));
+                eventMessage.BlocksToRender.Add(("test new tab", new Microsoft.AspNetCore.Html.HtmlString($"<div>TEST {categoryId}</div>")));
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
